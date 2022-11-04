ec2c22b241d124170a23fe27fbe8c681db046283
Bustarde <bustarde.audriel@auf.edu.ph>
2022-11-04 16:36:46 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//object
+let car = {
+    Brand : "Toyota",
+    Variant : "Wigo",
+    Transmission : "Automatic",
+    Lights : {
+        Front : "LED",
+        Rear : "LED/Bulb",
+        Signal : "Bulb"
+    },
+    displayBrand(){
+        alert(this.Brand);
+
+    }
+}
+
+//class
+class myCar {
+    constructor (brand, variant, transmission){
+        this.brand = brand; 
+        this.variant = variant; 
+        this.transmission = transmission; 
+    }
+}
